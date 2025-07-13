cda55a4c67a88752a56b4cdbb6eaf6d7b667e088
luciano1633 <luciano.gb.1633@gmail.com>
2025-07-13 17:56:13 -0400

    Delete msrabbitmq directory

--- msrabbitmq/rabbitmq-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rabbitmq-server -detached
-sleep 10
-rabbitmqctl stop_app
-rabbitmqctl reset
-rabbitmqctl join_cluster rabbit@rabbitmq1
-rabbitmqctl start_app
-
-tail -f /var/log/rabbitmq/rabbit@rabbitmq2.log
